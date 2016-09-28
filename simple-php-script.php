3b08a3802c2ef8b907118e356014597238ab2a37
ndmanvar <ndmanvar@yahoo.com>
2016-09-28 13:25:50 -0700

    simple php script example

---
+++ simple-php-script.php
@@ -0,0 +1,17 @@
+<?php
+  require_once('vendor/autoload.php');
+  use Facebook\WebDriver\Remote\RemoteWebDriver;
+  use Facebook\WebDriver\WebDriverBy;
+
+  $web_driver = RemoteWebDriver::create(
+    "https://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:443/wd/hub",
+    array("platform"=>"Windows 7", "browserName"=>"chrome", "version"=>"40")
+  );
+  $web_driver->get("https://saucelabs.com/test/guinea-pig");
+
+  /*
+    Test actions here...
+  */
+
+  $web_driver->quit();
+?>
